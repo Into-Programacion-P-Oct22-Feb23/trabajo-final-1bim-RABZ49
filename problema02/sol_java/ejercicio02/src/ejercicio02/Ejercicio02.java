48dba2bb27921997b4137b79a0cd79b13d1ea62b
Denis Ruiz <denisruiz112789@gmail.com>
2022-11-29 16:38:44 -0500

    trabajo final

--- problema02/sol_java/ejercicio02/src/ejercicio02/Ejercicio02.java
+++ problema02/sol_java/ejercicio02/src/ejercicio02/Ejercicio02.java
@@ -4,6 +4,9 @@
  */
 package ejercicio02;
 
+import java.util.Scanner;
+import java.util.Locale;
+
 /**
  *
  * @author Usuario iTC
@@ -15,6 +18,51 @@ public class Ejercicio02 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        int trajes;
+        double precio_unitario;
+        double subtotal = 0;
+        double total = 0;
+        int descuento = 0;
+        System.out.println("Ingrese la cantidad de trajes");
+        trajes = entrada.nextInt();
+        System.out.println("Ingrese el precio unitario del traje");
+        precio_unitario = entrada.nextDouble();
+        
+        if (trajes == 1) {
+            descuento = 20;
+            subtotal = precio_unitario;
+            total = (precio_unitario * descuento) / 100;
+            
+        } else {
+            if (trajes == 2) {
+                descuento = 25;
+                subtotal = precio_unitario * trajes;
+                total = (precio_unitario * descuento) / 100;
+                
+            } else {
+                if (trajes == 3) {
+                    descuento = 40;
+                    subtotal = precio_unitario * trajes;
+                    total = (precio_unitario * descuento) / 100;
+                    
+                } else {
+                    if (trajes > 3) {
+                        descuento = 50;
+                        subtotal = precio_unitario * trajes;
+                        total = (precio_unitario * descuento) / 100;
+                    }
+
+                }
+
+            }
+
+        }
+        System.out.printf("Los trajes comprados: %d\nPrecio unitario "
+                + "es de: %.2f\nSubtotal: %.2f\nEl descuento es del: %d "
+                + "por ciento\nTotal: %.2f\n", trajes, precio_unitario,
+                subtotal, descuento, total);
     }
-    
+
 }
