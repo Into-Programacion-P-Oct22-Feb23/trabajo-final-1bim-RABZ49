122ae606ed51289ef3f0b62e6d81e3ed5783b821
Roberto Bustamante <rabustamante5@utpl.edu.ec>
2022-11-30 13:51:33 -0500

    final

--- problema08/sol_python/problema08.py
+++ problema08/sol_python/problema08.py
@@ -1,23 +1,19 @@
 control = ""
 control1 = ""
+cadena_final = ""
 control2 = ""
 control3 = ""
 bandera = True
 salir = str("o")
-valores = "metros\tyardas\tpulgadas\tpies"
+valores = "\t\tmetros\tyardas\t\tpulgadas\tpies"
 while bandera:
     metros = float(input("ingrese el valor en metros:"))
     yardas = metros * 1.094
     pulgadas = metros * 39.37
     pies = metros * 3.281
-    yardasR = round(yardas, 2)
-    pulgadasR = round(pulgadas, 2)
-    piesR = round(pies, 2)
-    control = str(control) + str(metros) + "\n"
-    control1 = str(control1) + str(yardasR) + "\n"
-    control2 = str(control2) + str(pulgadasR) + "\n"
-    control3 = str(control3) + str(piesR) + "\n"
+    cadena_final = "%s\t\t%s\t\t%s\t\t%s\t\t%s\n" % (cadena_final, metros, round(yardas, 2), round(pulgadas, 2), round(pies, 2))
     salir = input("ingrese s para salir.\n-->")
     if salir == "s":
         bandera = False
-print("\nmetros\n\n" + control + "\nyardas\n\n" + control1 + "\npulgadas\n\n" + control2 + "\npies\n\n" + control3)
+print(valores)
+print(cadena_final)
