ac060acca48cfb8fcd8b359044d9179f20660209
Roberto Bustamante <rabustamante5@utpl.edu.ec>
2022-11-29 21:19:53 -0500

    final

--- problema10/sol_python/Problema10.py
+++ problema10/sol_python/Problema10.py
@@ -1,17 +1,16 @@
-import numpy as np
-
 bandera = True
 dato1 = 0
 dato2 = 0
 mensaje = 0
+contador = 0
 media = 0
 control = "b"
 while bandera:
     dato1 = int(input("ingrese el dato a calcular:"))
-    mensaje = mensaje , dato1
-    dato2 = mensaje
+    mensaje = mensaje + dato1
     control = input("ingrese s para salir\n-->")
+    contador = contador + 1
     if control == "s":
         bandera = False
-print(mensaje)
-print(np.mean(mensaje))
+media = mensaje/contador
+print("La media es de :"media)
